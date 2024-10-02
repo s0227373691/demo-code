9ccf922f73c785185326a49ffbe610215784953f
Elmer <s0227373691@gms.tku.edu.tw>
2024-10-02 13:22:41 +0800

    add style

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,9 @@
 import React from "react";
-import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
 const Layout = ({ children }) => {
-  return (
-    <Container>
-      <Row className="d-flex flex-fill">{children}</Row>
-    </Container>
-  );
+  return <div className="d-flex w-100">{children}</div>;
 };
 
 function LayoutLeft({ children }) {
