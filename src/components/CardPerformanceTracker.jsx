82849839beab440d348c57128b7736740fcb3ba5
Elmer <s0227373691@gms.tku.edu.tw>
2024-10-02 23:33:26 +0800

    fix: attribute name

--- src/components/CardPerformanceTracker.jsx
+++ src/components/CardPerformanceTracker.jsx
@@ -33,14 +33,14 @@ function Icon({ className }) {
       <path
         d="M13.3867 2.37109V5.02163"
         stroke="#2D308A"
-        stroke-width="1.33333"
-        stroke-miterlimit="10"
+        strokeWidth="1.33333"
+        strokeMiterlimit="10"
       />
       <path
         d="M2.72428 12.2389H6.87667V22.9056H2.72428V12.2389Z"
         fill="#2D308A"
         stroke="#2D308A"
-        stroke-width="1.33333"
+        strokeWidth="1.33333"
       />
       <path
         d="M6.12249 23.9634V25.7633H0V14.9062H1.59453V23.9634H6.12249Z"
@@ -50,7 +50,7 @@ function Icon({ className }) {
         d="M10.9528 6.23893H15.1052V22.9056H10.9528V6.23893Z"
         fill="#2D308A"
         stroke="#2D308A"
-        stroke-width="1.33333"
+        strokeWidth="1.33333"
       />
       <path
         d="M14.351 23.962V25.762H8.22852V10.2383H9.82304V23.962H14.351Z"
@@ -60,7 +60,7 @@ function Icon({ className }) {
         d="M19.1813 8.90495H23.3337L23.3337 22.9049H19.1813L19.1813 8.90495Z"
         fill="#2D308A"
         stroke="#2D308A"
-        stroke-width="1.33333"
+        strokeWidth="1.33333"
       />
       <path
         d="M22.5795 23.9627V25.7626H16.457V11.5723H18.0516V23.9627H22.5795Z"
